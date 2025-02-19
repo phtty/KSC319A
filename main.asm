74ed10332146355cd93984411ad7b67c965e718b
魏懿航 <dzzwmqj@outlook.com>
2025-02-19 16:29:59 +0800

    feat: 显示部分基础功能完成，按键驱动完成

--- main.asm
+++ main.asm
@@ -17,23 +17,9 @@ START:
 
 	call	F_ClearScreen
 	
-	bsf		Clock_Flag,0
-
-	; movlw	11
-	; movwf	R_Time_Min
-	; movlw	22
-	; movwf	R_Time_Hour
-	; call	L_DisTime_Min
-	; call	L_DisTime_Hour
-	movlw	25
-	movwf	R_Date_Year
-	movlw	2
-	movwf	R_Date_Month
-	movlw	18
-	movwf	R_Date_Day
-	call	L_DisDate_Year
-MAIN:
 	
+MAIN:
+	call	F_KeyHandler
 	goto	MAIN
 
 
@@ -76,5 +62,6 @@ include KeyHandler.inc
 include LedTable.inc
 include Dis.inc
 include Display.inc
+include Temper.inc
 
 end
